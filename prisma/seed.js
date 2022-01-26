0231d18133ad191eb5f62120aa21a6781509844c
bravint <bravint@gmail.com>
2022-01-26 16:18:43 +0000

    requirement 3 completed

--- prisma/seed.js
+++ prisma/seed.js
@@ -22,8 +22,27 @@ async function seed() {
     });
 
     console.log('contact created', createdContact);
+
+    const createdMovie = await prisma.movie.create({
+        data: {
+            title: `Jurassic Park`,
+            runtimeMins: 200
+        }
+    })
+
+    console.log('movie created', createdMovie);
+
+    const createdScreening = await prisma.screening.create({
+        data: {
+            startsAt: "2022-03-19T14:21:00+00:00"
+        }
+    })
+
+    console.log('screening created', createdScreening);
+
+
     // Don't edit any of the code below this line
-    process.exit(0);
+    process.exit(0); 
 }
 
 seed().catch(async (error) => {
